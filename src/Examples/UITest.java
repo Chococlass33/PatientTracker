e4bd37fea6de5f0c489c5eff1ca404b1b7e296b2
Justin Heaton <jhea0008@student.monash.edu>
2020-05-09 20:19:36 +1000

    Added code for an example UI Test
    Added code to create a sample UI showcasing table views. Also moved requestExample to the examples package

---
+++ src/Examples/UITest.java
@@ -0,0 +1,29 @@
+package Examples;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UITest extends Application{
+    private TableView table = new TableView();
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button addPatientButton = new Button("Add new Patient");
+        stage.setTitle("Hello");
+        TableColumn nameColumn = new TableColumn("NAME");
+        TableColumn cholesterolColumn = new TableColumn("TOTAL CHOLESTEROL");
+        TableColumn timeColumn = new TableColumn("TIME");
+        table.getColumns().addAll(nameColumn, cholesterolColumn, timeColumn);
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(addPatientButton, table);
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
--- src/Examples/requestExample.java
+++ src/Examples/requestExample.java
@@ -1,3 +1,5 @@
+package Examples;
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.r4.model.*;
